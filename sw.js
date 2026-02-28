a4eb82299ce6ff92acf7fa529ef1309f51e044ae
quowavy <dir.oskv@icloud.com>
2026-02-28 18:11:58 -0500

    Fix service worker cache paths for subpath deployment

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE = 'camerafx-v1';
-const ASSETS = ['/', '/index.html', '/manifest.json', '/icon-192.png', '/icon-512.png'];
+const CACHE = 'camerafx-v2';
+const ASSETS = ['/camerafx/', '/camerafx/index.html', '/camerafx/manifest.json', '/camerafx/icon-192.png', '/camerafx/icon-512.png'];
 
 self.addEventListener('install', e => {
   e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)));
